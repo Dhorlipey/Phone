fd83501d1f18f5b19c6fbf2533b9c410557c8844
Oluwadolapor <dolapoonifade03@gmail.com>
2022-05-13 11:48:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function phoneOn() {
+
+  const live = document.getElementById("screenview");
+
+  if (live.style.opacity = "0") {
+    live.style.opacity = "1"
+  }
+
+  else if (live.style.opacity = "1") {
+    live.style.opacity = "0"
+  }
+
+
+
+}
+
